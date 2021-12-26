569fd1ef144e98e099b566dc8690782ae399b411
iAmAli <ali.naderi72@gmail.com>
2021-12-26 10:55:52 +0330

    added routes and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css'
+// tools
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// components
+import Home from './pages/home/Home';
+import Search from './pages/search/Search';
+import Create from './pages/create/Create';
+import Recipe from './pages/recipe/Recipe';
+
+// styles
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/search' element={ <Search /> } />
+          <Route path='/create' element={ <Create /> } />
+          <Route path='/recipes/:id' element={ <Recipe /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
